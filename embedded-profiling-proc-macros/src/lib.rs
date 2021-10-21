20afb8c0e81e6fd3b36b6212c7cd026aaf2b052a
Tyler Holmes <tyler@holmesengineering.com>
2021-10-21 16:05:42 -0500

    proc macro progress but doesn't work

--- embedded-profiling-proc-macros/src/lib.rs
+++ embedded-profiling-proc-macros/src/lib.rs
@@ -5,12 +5,25 @@ use quote::quote;
 use syn::{parse_macro_input, parse_quote, ItemFn};
 
 #[proc_macro_attribute]
-pub fn function(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn profile(attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut function = parse_macro_input!(item as ItemFn);
+    // let input_struct = proc_macro2::TokenStream::from(attr);
+    let input_struct = parse_macro_input!(attr as syn::ItemStatic);
     let instrumented_function_name = function.sig.ident.to_string();
 
     let body = &function.block;
-    let new_body: syn::Block = impl_block(body, &instrumented_function_name);
+    let new_body: syn::Block = parse_quote! {
+        {
+            #input_struct;
+            let derp = #input_struct::get();
+            let start = derp.start_snapshot();
+
+            #body
+
+            let dur = derp.end_snapshot(start, #instrumented_function_name);
+            #input_struct::borrow_writer(|writer| writeln!(writer, "{}", dur).unwrap());
+        }
+    };
 
     function.block = Box::new(new_body);
 
@@ -18,14 +31,4 @@ pub fn function(_attr: TokenStream, item: TokenStream) -> TokenStream {
         #function
     })
     .into()
-}
-
-fn impl_block(body: &syn::Block, instrumented_function_name: &str) -> syn::Block {
-    parse_quote! {
-        {
-            let derp = EmbeddedTrace::new(#instrumented_function_name);
-
-            #body
-        }
-    }
 }
