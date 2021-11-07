82368077ca411c57247a73b29181641bb96b64e1
Tyler Holmes <tyler@holmesengineering.com>
2021-11-07 15:27:16 -0800

    make example profiler more efficient with some compile time GCD magic

--- embedded-profiling/src/dwt_systick.rs
+++ embedded-profiling/src/dwt_systick.rs
@@ -36,13 +36,66 @@ impl<const FREQ: u32> DwtSystick<FREQ> {
 
         timer
     }
+
+    /// binary GCD function stolen from wikipedia, made const
+    const fn gcd(mut u: u32, mut v: u32) -> u32 {
+        // Base cases: gcd(n, 0) = gcd(0, n) = n
+        if u == 0 {
+            return v;
+        } else if v == 0 {
+            return u;
+        }
+
+        // Using identities 2 and 3:
+        // gcd(2ⁱ u, 2ʲ v) = 2ᵏ gcd(u, v) with u, v odd and k = min(i, j)
+        // 2ᵏ is the greatest power of two that divides both u and v
+        let i = u.trailing_zeros();
+        u >>= i;
+        let j = v.trailing_zeros();
+        v >>= j;
+
+        // min(i, j);
+        let k = if i <= j { i } else { j };
+
+        loop {
+            // u and v are odd at the start of the loop
+            // debug_assert!(u % 2 == 1, "u = {} is even", u);
+            // debug_assert!(v % 2 == 1, "v = {} is even", v);
+
+            // Swap if necessary so u <= v
+            if u > v {
+                // swap(&mut u, &mut v);
+                let tmp = u;
+                u = v;
+                v = tmp;
+            }
+
+            // Using identity 4 (gcd(u, v) = gcd(|v-u|, min(u, v))
+            v -= u;
+
+            // Identity 1: gcd(u, 0) = u
+            // The shift by k is necessary to add back the 2ᵏ factor that was removed before the loop
+            if v == 0 {
+                return u << k;
+            }
+
+            // Identity 3: gcd(u, 2ʲ v) = gcd(u, v) (u is known to be odd)
+            v >>= v.trailing_zeros();
+        }
+    }
+
+    /// Reduce the fraction we need to convert between 1µs precision and whatever our core clock is running at
+    pub(crate) const fn reduced_fraction() -> (u32, u32) {
+        let gcd = Self::gcd(1_000_000, FREQ);
+        (1_000_000 / gcd, FREQ / gcd)
+    }
 }
 
 impl<const FREQ: u32> crate::EmbeddedProfiler for DwtSystick<FREQ> {
     fn read_clock(&self) -> crate::EPInstant {
-        // TODO: fix this gross conversion
+        let (red_num, red_denom) = Self::reduced_fraction();
         crate::EPInstant::from_ticks(
-            (self.dwt.cyccnt.read() as u64 * 1_000_000_u64 / FREQ as u64) as crate::EPContainer,
+            (self.dwt.cyccnt.read() * red_num / red_denom) as crate::EPContainer,
         )
     }
 
